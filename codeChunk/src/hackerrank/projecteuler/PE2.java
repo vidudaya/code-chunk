9cc53953467809aab35edf731160b4c07cdd1652
Vidudaya Bandara <vnbandara@gmail.com>
2014-11-02 12:11:34 +0530

    PE2

---
+++ codeChunk/src/hackerrank/projecteuler/PE2.java
@@ -0,0 +1,29 @@
+/*
+ * Project Euler #1: Multiples of 3 and 5
+ * 
+ */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.*;
+
+public class PE2 {
+
+    public static void main(String[] args) throws IOException {
+        new PE2().solve();
+    }
+
+    public void solve() throws IOException {
+
+        Scanner sc = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter pw = new PrintWriter(System.out);
+
+    }
+
+    public static Integer toInt(String s) {
+        return Integer.parseInt(s.trim());
+    }
+}
